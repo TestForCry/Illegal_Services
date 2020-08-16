394dd9faaacde593b7673dd4bec72f6a0e0c3ea8
Illegal Services <62464560+Illegal-Services@users.noreply.github.com>
2020-08-16 17:32:26 +0200

    Update nmap.bat

--- NMAP/nmap.bat
+++ NMAP/nmap.bat
@@ -1,11 +1,11 @@
 @echo off
 net start npcap
 cls
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v MaxUserPort /t REG_DWORD /d 64534 /f
+call "reg.exe" add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v MaxUserPort /t REG_DWORD /d 64534 /f
 cls
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v StrictTimeWaitSeqCheck /t REG_DWORD /d 1 /f
+call "reg.exe" add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v StrictTimeWaitSeqCheck /t REG_DWORD /d 1 /f
 cls
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v TcpTimedWaitDelay /t REG_DWORD /d 30 /f
+call "reg.exe" add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v TcpTimedWaitDelay /t REG_DWORD /d 30 /f
 cls
 mode con lines=29999 cols=87
 title ^|NMAP :  %choice%^|    ^|%option1%^|    ^|%option2%^|
@@ -15,9 +15,11 @@ echo        #################################################################
 echo        #       [40;91m[40;90m   [40;36mWelcome in the best Port Scanner by NMAP   [40;91m[40;90m        #
 echo        #################################################################
 echo:
-echo         [40;36m%option1%[40;90m
-echo         [40;36m%option2%[40;90m
-echo         [40;36mThat's all, now just relax and whait . . .[40;90m
+echo         [40;36mScan : %option1%[40;90m
+if "%language%"=="FR" echo         [40;36mTemps : %option2%[40;90m
+if "%language%"=="EN" echo         [40;36mTime : %option2%[40;90m
+if "%language%"=="FR" echo         [40;36mVeuillez patienter pendant le scan de %choice% . . .[40;90m
+if "%language%"=="EN" echo         [40;36mPlease wait while scanning %choice% . . .[40;90m
 echo:
 echo =======================================================================================
 echo [40;31m
@@ -25,6 +27,9 @@ call "Portable_Apps\NMAP\nmap.exe" -T5 %prefix%%argument1%%middle%%argument2% %c
 echo [40;90m
 echo =======================================================================================
 echo [40;36m
-echo Press any key to exit . . .
+if "%language%"=="FR" echo         Scan terminer.
+if "%language%"=="EN" echo         Scan finished.
+if "%language%"=="FR" echo         Appuyez sur une touche pour quitter.
+if "%language%"=="EN" echo         Press any key to exit.
 pause >nul
 exit
