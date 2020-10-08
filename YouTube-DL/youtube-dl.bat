703bc0aae546c62be916271fbe662cf4f695132c
Illegal-Services <>
2020-10-08 20:21:02 +0200

    Update youtube-dl.bat

--- YouTube-DL/youtube-dl.bat
+++ YouTube-DL/youtube-dl.bat
@@ -20,10 +20,11 @@ call "Portable_Apps\YouTube-DL\youtube-dl.exe" --config-location "Portable_Apps\
 echo [40;90m
 echo ==========================================================================================================
 echo [40;36m
-if "%language%"=="FR" echo         T‚l‚chargement terminer.
-if "%language%"=="EN" echo         Download finished.
-if "%language%"=="FR" echo         Appuyez sur une touche pour ouvrir l'emplacement de t‚l‚chargement.
-if "%language%"=="EN" echo         Press any key to open download location.
+if %errorlevel% EQU 0 (if "%language%"=="FR" echo         T‚l‚chargement terminer.) ELSE (echo une erreur s'est produite et n'a pas pu t‚l‚charger le fichier.)
+if %errorlevel% EQU 0 (if "%language%"=="EN" echo         Download finished.) ELSE (echo something went wrong and couldn't download the file.)
+if %errorlevel% EQU 0 (if "%language%"=="FR" echo         Appuyez sur une touche pour ouvrir l'emplacement de t‚l‚chargement.) ELSE (echo Appuyez sur une touche pour fermer.)
+if %errorlevel% EQU 0 (if "%language%"=="EN" echo         Press any key to open download location.) ELSE (echo Press any key to exit.)
 pause >nul 2>&1
+if %errorlevel% NEQ 0 exit
 if defined output if exist %output% start "" "explorer.exe" "%output%"
 exit
